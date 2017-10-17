74b8ec53ede09ea965f4ef948ba39547220c5ad9
Jaroslav Páral <paral.jarek@gmail.com>
2017-10-17 14:32:14 +0200

    Install script for new PC used Chocolatey

---
+++ install_chocolatey_python_code.bat
@@ -0,0 +1,4 @@
+choco install -y firefox googlechrome 7zip python3 eagle visualstudiocode vlc git tortoisegit hg tortoisehg cygwin pip kicad mobaxterm altap-salamander inkscape gimp adobereader pspad   
+pip install pygame pyserial numpy matplotlib py2exe
+code --install-extension ms-vscode.cpptools
+code --install-extension platformio.platformio-ide 
