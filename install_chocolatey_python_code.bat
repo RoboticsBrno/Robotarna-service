de6cd6962b4cd8092941830ba79816ed8d298b9d
Jaroslav Páral <paral.jarek@gmail.com>
2017-10-25 11:30:55 +0200

    Add new packages to chocolatey script (editors, IDEs, tools)

--- install_chocolatey_python_code.bat
+++ install_chocolatey_python_code.bat
@@ -1,4 +1,4 @@
-choco install -y firefox googlechrome 7zip python3 eagle visualstudiocode vlc git tortoisegit hg tortoisehg cygwin pip kicad mobaxterm altap-salamander inkscape gimp adobereader pspad   
+choco install -y firefox googlechrome 7zip python3 eagle visualstudiocode vlc git tortoisegit hg tortoisehg cygwin pip kicad mobaxterm altap-salamander inkscape gimp adobereader pspad notepadplusplus libreoffice pdfcreator miktex texstudio pandoc arduino clion-ide pycharm-community f.lux mobaxterm winscp 
 pip install pygame pyserial numpy matplotlib py2exe
 code --install-extension ms-vscode.cpptools
 code --install-extension platformio.platformio-ide 
