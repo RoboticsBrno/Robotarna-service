1b3fbe33e9c34995d43113f68b606e7389cd23a6
Jaroslav Páral <paral.jarek@gmail.com>
2017-11-03 11:49:54 -0700

    Create install_chocolatey_sokolska_ntb.bat

---
+++ install_chocolatey_sokolska_ntb.bat
@@ -0,0 +1,7 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install -y teamviewer firefox googlechrome flashplayerplugin jre8 javaruntime treesizefree 7zip python3 eagle visualstudiocode vlc git tortoisegit hg tortoisehg cygwin pip skype kicad irfanview mobaxterm doublecmd inkscape gimp adobereader pspad notepadplusplus libreoffice pdfcreator miktex texstudio pandoc arduino clion-ide pycharm-community f.lux mobaxterm winscp poweriso atom autodesk-fusion360
+
+pip install pygame pyserial numpy matplotlib py2exe
+code --install-extension ms-vscode.cpptools
+code --install-extension platformio.platformio-ide 
