ee204d791ee6a529bef9fd0926ce9bc90935c465
Jaroslav Páral <paral.jarek@gmail.com>
2018-02-10 12:12:56 +0100

    install_linux_script: update Python packeges

--- install_linux_script.sh
+++ install_linux_script.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
-sudo apt-get install arduino atom chromium-browser clang code curl eagle fritzing gparted htop idle-python2.7 idle-python3.4 imagemagick-common kicad libmagick++5 libmagickcore5 libmagickwand5 lorris mc python-dev python-pip qt-sdk sublime-text tortoisehg tortoisehg-nautilus openssh-client openssh-server sl screen zsh adduser g++ gcc-4.9-base gdb gedit git git-core grep gzip iftop initramfs-tools nano oracle-java8-jdk p7zip p7zip-full parted perl perl-base tar tightvncserver unzip usb-modeswitch usbutils util-linux v4l-utils vim vim-common vim-tiny wget xinit xrdp zip zsh
+sudo apt-get install arduino atom chromium-browser clang code curl eagle fritzing gparted htop idle-python2.7 idle-python3.4 imagemagick-common kicad libmagick++5 libmagickcore5 libmagickwand5 lorris mc python-dev python-pip python-pygame python-serial python-numpy python-matplotlib python3-serial python3-numpy python3-matplotlib python3-pip qt-sdk sublime-text tortoisehg tortoisehg-nautilus openssh-client openssh-server sl screen zsh adduser g++ gcc-4.9-base gdb gedit git git-core grep gzip iftop initramfs-tools nano oracle-java8-jdk p7zip p7zip-full parted perl perl-base tar tightvncserver unzip usb-modeswitch usbutils util-linux v4l-utils vim vim-common vim-tiny wget xinit xrdp zip zsh
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get autoremove
 
-pip install pygame pyserial numpy matplotlib py2exe
+pip3 install pygame
